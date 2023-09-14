fe10dbd769c927889a19935999d7573605b2015c
cmpvsolar <cmpvsolar@gmail.com>
2023-09-14 14:28:29 -0700

    reg update

--- app/Controllers/Asiaguest.php
+++ app/Controllers/Asiaguest.php
@@ -36,7 +36,7 @@ die();
 
 public function customers()
 {
-	$db = \Config\Database::connect('default');
+	$db = \Config\Database::connect('registration');
 	//$db->setDatabase('bitswork_registration');
     $crud = $this->_getGroceryCrudEnterprise();
 
