41b3e178778b2a667b5dc0b0ef1968c45556c648
cmpvsolar <cmpvsolar@gmail.com>
2023-09-16 16:27:51 -0700

    umcomment action buttons

--- app/Controllers/Asiaguest.php
+++ app/Controllers/Asiaguest.php
@@ -176,8 +176,8 @@ public function contact585442()
 	
 	//Don't set so default update occurs $this->grocery_crud->field_type('Stamp','hidden');
 	
-	/* $crud->setActionButton('Edit Master Contact','','','ui-icon-contact',array($this,'edit_master_contact_URL'));
-	$crud->setActionButton('Print China2020', '', site_url('/china/singleprintchina/'),'ui-icon-image'); */
+	$crud->setActionButton('Edit Master Contact','','','ui-icon-contact',array($this,'edit_master_contact_URL'));
+	$crud->setActionButton('Print China2020', '', site_url('/china/singleprintchina/'),'ui-icon-image');
 	
 	//,array($this,'Meptecinvoice'));
 	//$crud->add_action('Print Label', '', site_url('/china/Meptecsinglelabel/'),'ui-icon-image');
