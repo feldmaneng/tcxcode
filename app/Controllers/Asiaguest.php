40b0555616d4d53c99ac754450239d3ad0d16a8d
cmpvsolar <cmpvsolar@gmail.com>
2023-09-22 14:28:40 -0700

    attempt get first row

--- app/Controllers/Asiaguest.php
+++ app/Controllers/Asiaguest.php
@@ -523,7 +523,7 @@ public function guest_list()
   // $crud->where(['SecretKey'=> $secretKey]);
    //look into get and query statements
    	$query = $crud->getWhere(['SecretKey'=> $secretKey],1,1);
-	$row = $query->getResult(0);
+	$row = $query->getFirstRow();
 	
 	if ($crud->countAllResults()!= 1) {
 		sleep(20); /* slow down a brute force */ 
@@ -532,6 +532,7 @@ public function guest_list()
 		echo "</pre>";
 		die();
 	}
+	
 	//ask ira
 	$companyID = $row->CompanyID;
 	$_SESSION["CompanyID"] = $companyID; 
