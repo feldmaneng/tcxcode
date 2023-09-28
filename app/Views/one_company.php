b6efb5b14f9086e17a28b9ac36a2bafb1f010579
cmpvsolar <cmpvsolar@gmail.com>
2023-09-27 19:12:55 -0700

    update asia guest

--- app/Views/one_company.php
+++ app/Views/one_company.php
@@ -3,16 +3,14 @@
 <head>
     <meta charset="utf-8" />
  
-<?php 
-
-foreach($css_files as $file): ?>
-    <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
- 
-<?php endforeach; ?>
-<?php foreach($js_files as $file): ?>
- 
-    <script src="<?php echo $file; ?>"></script>
-<?php endforeach; ?>
+<?php
+if (!empty($css_files)) {
+foreach($css_files as $file) { ?>
+ <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+<?php
+}
+}
+?>
  
 <style type='text/css'>
 body
@@ -34,31 +32,65 @@ a:hover
 <body>
 <!-- Beginning header -->
     <div>
+<?php 
+$session = session();
 
-        <!-- <a href='<?php echo site_url('main2/company')?>'>Company List</a> | 
-        <a href='<?php echo site_url('examples/employees_management')?>'>Employees</a> |
-        <a href='<?php echo site_url('examples/customers_management')?>'>Customers</a> |
-        <a href='<?php echo site_url('examples/orders_management')?>'>Orders</a> |
-        <a href='<?php echo site_url('examples/products_management')?>'>Products</a> | 
-        <a href='<?php echo site_url('examples/film_management')?>'>Films</a>
- 		-->
- 		
- 		Secret Key:  <? echo $_SESSION["SecretKey"] ?><br>
- 		<h1><? echo $_SESSION["Event"] ?></h1>
-   		<h2><? echo $_SESSION["Company"] ?></h2>
-   		<p>TestConX EXPO Staff 展商员工: <? echo $_SESSION["StaffName"] ?><br>
+$secretKey= $_SESSION["SecretKey"];
+$event = $_SESSION["Event"];
+$company = $_SESSION["Company"];
+$staffName = $_SESSION["StaffName"];
+$guestLimit = $_SESSION["GuestLimit"];
+$output2 = $_SESSION["Output"];
+echo $_SESSION["SecretKey"];
+echo  $_SESSION["Event"];
+echo  $_SESSION["Company"];
+echo $_SESSION["StaffName"];
+echo $_SESSION["GuestLimit"];
+
+
+$var = "Hello World!";
+$html = <<<EOT
+<div>
+   Secret Key:   
+		
+		
+		$secretKey
+		
+		<br>
+ 		<h1> $event; </h1>
+   		<h2> 	 $company</h2>
+   		<p>TestConX EXPO Staff 展商员工: $staffName <br>
    		(The person in charge of your booth. They will receive a Full Conference registration.<br>
    		该人员为负责贵展位的直接联系人，将获得全场会议通行证。)</p>
-   		<p>You are entitled to invite <? echo $_SESSION["GuestLimit"] ?> guests to receive complimentary <b>Full Conference</b> registration.
+   		<p>You are entitled to invite $guestLimit guests to receive complimentary <b>Full Conference</b> registration.
    			For customers, staff, and other guests.<br>
-   			贵司有权邀请<? echo $_SESSION["GuestLimit"] ?>张额外全场会议通行证，供贵司员工、客户或相关客人入场。</p>
+   			贵司有权邀请  $guestLimit 张额外全场会议通行证，供贵司员工、客户或相关客人入场。</p>
     </div>
+</div>
+EOT;
+echo $html;
+
+//print_r($output->output);
+//echo $output;
+//echo $output2;
+ 
+     if (!empty($output)) {
+         echo $output;
+     }
+     
+ ?>
+ <?php
+if (!empty($js_files)) {
+    foreach($js_files as $file) { ?>
+        <script src="<?php echo $file; ?>"></script>
+    <?php }
+}
+?>
+ 		
+ 		
 <!-- End of header-->
     <div style='height:20px;'></div>  
-    <div>
-<?php echo $output; ?>
- 
-    </div>
+   
 <!-- Beginning footer -->
 <div><!--Footer--></div>
 <!-- End of Footer -->
