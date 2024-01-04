791edf865b42110fb50a5ab95b3698f6870b217b
cmpvsolar <cmpvsolar@gmail.com>
2024-01-04 14:42:43 -0800

    exhibitor updates fixing error page

--- app/Views/upload_error.php
+++ app/Views/upload_error.php
@@ -8,17 +8,17 @@
 
 //$this->load->library('session');
 $session = session(); 
-$data = array('upload_data' => $this->upload->data());
-$a=$this->upload->data();  
-$error_name = $a['file_name'];	
-
-echo "<h2>Error uploading your file - $error_name</h2>";
-echo "<h2>Error: " . $error . "</h2>";
+//$data = array('upload_data' => $this->upload->data());
+//$a=$this->upload->data();  
+//$error_name = $a['file_name'];	
+//var_dump($error);
+echo "<h2>Error uploading your file</h2>";
+echo "<h2>Error: " . $error['userfile'] . "</h2>";
 echo "<h2>Please make sure your file size is not larger than 1 megabyte.</h2>";
 echo "<h2>Please contact TestConX if you are not able to upload your file.</h2>";
 
-echo "<a href=\"javascript:history.go(-1)\">Return to TestConX EXPO Workshop Guide</a>";
 
+echo anchor('directory?key='.session('secretKey'), 'Return to TestConX EXPO Directory Entry'); 
 ?>
 
 </body>
