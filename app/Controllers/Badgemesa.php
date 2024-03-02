0a3995901f85ad4f0ab7918c37abe25e4e4ad8ac
cmpvsolar <cmpvsolar@gmail.com>
2024-03-01 18:54:39 -0800

    returned page to loop

--- app/Controllers/Badgemesa.php
+++ app/Controllers/Badgemesa.php
@@ -224,7 +224,7 @@ $pdf->SetDisplayMode('real', 'default');
 $pdf->setPrintHeader(false);
 $pdf->setPrintFooter(false);
 $graphics = TRUE;
-$pdf->AddPage('P',$pageLayout);
+//$pdf->AddPage('P',$pageLayout);
 		$q=0; 
 		$filler=0;
 		$pages=0;
@@ -258,7 +258,7 @@ for($i=1; $i<=$people; $i++){
 		$Dinner=$results[$n]["Dinner"];
 		$type = $results[$n]["Type"];
 		
-		//$pdf->AddPage('P',$pageLayout);
+		$pdf->AddPage('P',$pageLayout);
 		$Dinnertext="";
 		if($HardCopy==1){
 		$HardCopy="HC";
