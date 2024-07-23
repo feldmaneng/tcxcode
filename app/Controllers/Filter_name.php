e5201397423342ea826cb3c8ab1325bbb75be2ba
cmpvsolar <cmpvsolar@gmail.com>
2024-07-23 13:50:06 -0700

    test

--- app/Controllers/Filter_name.php
+++ app/Controllers/Filter_name.php
@@ -25,7 +25,7 @@ class Filter_name extends BaseController {
 	}
 		function filter(){
 			//the file should not have column names
-			$file = filter.csv;
+			$file = "filter.csv";
 			if(($handle = fopen($file,"r")) !==FALSE)	{
 				
 				$list = array_map('str_getcsv', file($file));
