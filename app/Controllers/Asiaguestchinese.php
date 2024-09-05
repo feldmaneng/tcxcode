a555eba5a4ef75973adefecc2ee9369c79992db5
cmpvsolar <cmpvsolar@gmail.com>
2024-09-04 20:52:28 -0700

    langauge test

--- app/Controllers/Asiaguestchinese.php
+++ app/Controllers/Asiaguestchinese.php
@@ -847,8 +847,8 @@ $builder->where('SecretKey', $secretKey);
 		 <div style="display:none">
 		 '
    );  */
-	$crud->setLanguagePath('grocery-crud/languages/spanish.php');
-	//$crud->setLanguage('Spanish');
+	//$crud->setLanguagePath('grocery-crud/languages/spanish.php');
+	$crud->setLanguage('Spanish');
 	//$crud->setLanguage("english-chinese");
 	$output = $crud->render();
 	$newdata = [
