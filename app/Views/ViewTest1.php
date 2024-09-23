c5cad2feb111e8bb44e23f1885d7f4a1cf6e636d
cmpvsolar <cmpvsolar@gmail.com>
2024-09-23 11:27:55 -0700

    print and view update

--- app/Views/ViewTest1.php
+++ app/Views/ViewTest1.php
@@ -9,11 +9,27 @@
 <h3> Here we will test variables below</h3>
 <h1><?= esc($var1) ?></h1>
 <h1><?= esc($var2) ?></h1>
-
+<button onclick="viewfunction()">Click to view</button>
+<button onclick="displayDate()">The time is?</button>
  <?php foreach ($var3 as $item): ?>
 
         <li><?= esc($item) ?></li>
 
     <?php endforeach ?>
+	<script>
+	function viewfunction(){
+		
+		const element = document.createElement('div');
+		element.innerHTML = "<p>Hi Bree you have a nice smile.♥</p>";
+		document.body.appendChild(element);
+		
+	}
+	
+		function displayDate() {
+	  document.getElementById("demo").innerHTML = Date();
+	}
+	
+	</script>
+	<p id="demo"></p>
 </body>
 </html>
