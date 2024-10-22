e5b35c1343ce8b8e56ed1d0f218173ed80895dd9
cmpvsolar <cmpvsolar@gmail.com>
2024-10-22 14:59:32 -0700

    test

--- app/Controllers/PrintBadge.php
+++ app/Controllers/PrintBadge.php
@@ -261,11 +261,11 @@ class PrintBadge extends BaseController
 				//here is where we need the fonts
 				
 				// convert TTF font to TCPDF format and store it on the fonts folder
-			$fontname = TCPDF_FONTS::addTTFfont(site_url('tcxcode/ThirdParty/NotoSerifKR-VariableFont_wght.ttf'), 'TrueTypeUnicode', '', 96);
+			//$fontname = TCPDF_FONTS::addTTFfont(site_url('tcxcode/ThirdParty/NotoSerifKR-VariableFont_wght.ttf'), 'TrueTypeUnicode', '', 96);
 
 			// use the font
 			if($EventYear == "korea2024"){
-			$pdf->SetFont($fontname, 'B', 55,);
+			$pdf->SetFont('cid0kr', 'B', 55,);
 			}
 			else if($EventYear == "china2024"){
 				$pdf->SetFont('stsongstdlight', 'B', 55,);
