4ea5360e968666ba9236e80ed46e47801d07e719
cmpvsolar <cmpvsolar@gmail.com>
2024-12-04 19:10:41 -0800

    fixing post

--- app/Views/expo_upload.php
+++ app/Views/expo_upload.php
@@ -9,14 +9,15 @@
 
 	<h3>EXPO Populate</h3>
 	
+	
 
-	<form action="EXPOpopulate/populate" method="POST">
-	<label for="year"> Current Event Year:</label>
-	<input type="number" id="year" name="year" min ="2000" max ="3000">
-	<label for="event"> Event(Mesa,Korea,China):</label>
-	<input type="text" id="event" name="event">
-		
-		<br><br>
+	
+	
+		<?= form_open('Authorpopulate/do_upload') ?>
+		<label for="year"> Current Event Year:</label>
+		<input type="number" id="year" name="year" min ="2000" max ="3000">
+		<label for="event"> Event(Mesa,Korea,China):</label>
+		<input type="text" id="event" name="event">
 		<input type="submit">
 
 	</form>
