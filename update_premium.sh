d3bd693facbd214367cc7f58013e797a7f65771c
Ira Feldman <iradave@gmail.com>
2025-01-07 12:53:12 -0800

    Create update_premium.sh
    Moved from Bitscode

---
+++ update_premium.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Do a compare with master on GitHub
+
+cd ~/public_html/premium
+#git pull -u -v origin master  -- not sure where the -u came from , but not valid
+git pull -v origin master
+
+
