14c43cdffb3fd7a63ef8c2ad5cb5d3c00d0969bb
cmpvsolar <cmpvsolar@gmail.com>
2025-02-11 00:50:57 -0800

    update database

--- app/Controllers/Database.php
+++ app/Controllers/Database.php
@@ -67,6 +67,8 @@ class Database extends BaseController {
 		echo "<LI>Manage <a href=" . site_url('/expo/contact') . ">EXPO CONTACTS</a></LI>";
 		echo "<LI>Manage <a href=" . site_url('/presentations') . ">Presentations Cruds</a></LI>";
 		
+		echo "<LI>Manage <a href=" . site_url('/Certificate') . ">Certificate</a></LI>";
+		echo "<LI>Manage <a href=" . site_url('/Smember') . ">Certificate</a></LI>";
 		echo "<LI>Manage <a href=" . site_url('/Guest/companychina34556672') . ">China Company Cruds</a></LI>";
 		echo "<LI>Manage <a href=" . site_url('/Guest/companykorea2346878438') . ">Korea Company Cruds</a></LI>";
 		echo "<LI>Manage <a href=" . site_url('/Guest/Chinastats') . ">China Company Stats</a></LI>";
