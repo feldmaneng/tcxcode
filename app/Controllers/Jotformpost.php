50618f9a514499d659e91b610adfbeaa0860e6ae
Ira Feldman <iradave@gmail.com>
2025-03-02 00:06:32 -0700

    Update Jotformpost.php
    typo

--- app/Controllers/Jotformpost.php
+++ app/Controllers/Jotformpost.php
@@ -84,7 +84,7 @@ class Jotformpost extends BaseController {
 			}			
 			
 			if (str_contains($fees,'Professional') ||
-				str_contains($fees, 'Upgrade Exhibitor')  {
+				str_contains($fees, 'Upgrade Exhibitor'))  {
 				$type = "Professional"; 
 			}
 
