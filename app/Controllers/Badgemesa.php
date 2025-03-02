b868e3e428e6c151b8073f469bff61c9536674bf
cmpvsolar <cmpvsolar@gmail.com>
2025-03-01 23:03:20 -0800

    update badges

--- app/Controllers/Badgemesa.php
+++ app/Controllers/Badgemesa.php
@@ -248,207 +248,191 @@ echo  $id;
 	
 	$results = $query->getResultArray();
 	
-	 $height = '152.4';
- 	$width = '101.6';
-	
-	// $height = '158.75';
-// 	$width = '107.95';
-	
-$pageLayout = array($width, $height);
+	// IRA POSITION
+				$height = '150';
+			$width = '101.6';
+			$pageLayout = array($width, $height);
 
 
 
-$pdf = new \TCPDF('P', 'MM',$pageLayout, true, 'UTF-8', false);
-$pdf->SetTitle('Badge Single');
-$pdf->SetMargins(10,10,10,10);
-$pdf->SetHeaderMargin(0);
-$pdf->SetTopMargin(0);
-$pdf->setFooterMargin(0);
-$pdf->SetAutoPageBreak(true);
-$pdf->SetAuthor('Author');
-$pdf->SetDisplayMode('real', 'default');
-$pdf->setPrintHeader(false);
-$pdf->setPrintFooter(false);
-$graphics = TRUE;
-//$pdf->AddPage('P',$pageLayout);
-		$q=0; 
-		$filler=0;
-		$pages=0;
-		
-for($i=1; $i<=$people; $i++){ 
-		$n = $i-1;
-		// Define what special labels go on the badges
-		$label ="0";
-		
-		
-		
-	
-		$NameOnBadge=$results[$n]["NameOnBadge"];
-		
-		$GivenName=$results[$n]["GivenName"];
-		echo "<h1>".$GivenName."</h1>";
-		
-		
-		//$CN_Company=$results[$n]["CN_Company"];
-		$FamilyName=$results[$n]["FamilyName"];
-		$EventYear=$results[$n]["EventYear"];
-		$Company=$results[$n]["Company"];
-		$ContactID=$results[$n]["ContactID"];
-		$InvitedByCompanyID=$results[$n]["InvitedByCompanyID"];
-		$HardCopy=$results[$n]["HardCopy"];
-		$Tutorial=$results[$n]["Tutorial"];
-		$Control=$results[$n]["Control"];
-		$Message=$results[$n]["Message"];
-		$Dinner=$results[$n]["Dinner"];
-		$type = $results[$n]["Type"];
-		$Email = $results[$n]["Email"];
-		
-		$pdf->AddPage('P',$pageLayout);
-		$Dinnertext="";
-		if($HardCopy==1){
-		$HardCopy="HC";
-		}
-		else{
-		$HardCopy="0";
-		}
-		if($EventYear == "tinyml2024"){
-			if($Tutorial==1){
-			$Tutorial="SYMPOSIUM";
+		$pdf = new \TCPDF('P', 'MM',$pageLayout, true, 'UTF-8', false);
+		$pdf->SetTitle('Badge Single');
+		$pdf->SetMargins(0,0,0,0);
+		$pdf->SetHeaderMargin(0);
+		$pdf->SetTopMargin(0);
+		$pdf->setFooterMargin(0);
+		$pdf->SetAutoPageBreak(true);
+		$pdf->SetAuthor('Author');
+		$pdf->SetDisplayMode('real', 'default');
+		$pdf->setPrintHeader(false);
+		$pdf->setPrintFooter(false);
+		$graphics = TRUE;
+		//$pdf->AddPage('P',$pageLayout);
+				$q=0; 
+				$filler=0;
+				$pages=0;
+				
+		for($i=1; $i<=$people; $i++){ 
+				$n = $i-1;
+			$time = date("d-m-y h:i:s");
+			
+			$data = [
+			'PrintTime' => $time,
+			];
+
+			$builder->where('ContactID', $results[$n]["ContactID"]);
+			$builder->update($data);
+			
+				// Define what special labels go on the badges
+				$label ="0";
+			if (!empty($results[$n]["NameOnBadge"])){
+				
+				$NameOnBadge=$results[$n]["NameOnBadge"];
+					
 			}
-			else{
-			$Tutorial="";
-			}
+			else {
+				
+				$NameOnBadge=$results[$n]["GivenName"];
+				
 			}
 			
-		if($EventYear == "tinyml2024"){
-			if($Dinner==1){
-			$Dinnertext="Dinner";
-			}
-			else{
-			$Dinnertext="";
-			}
-			}
-		if($EventYear == "emea2024"){
-			if($Tutorial==1){
-			$Tutorial="Social";
-			}
-			else{
-			$Tutorial="";
-			}
-			}	
-		if($EventYear == "emea2024"){
-			if($Dinner==1){
-			$Dinnertext="Dinner";
-			}
-			else{
-			$Dinnertext="";
-			}
-			}
-			
-			/* if($Tutorial==1){
-			$Tutorial="TUTORIAL";
-			}
-			else{
-			$Tutorial="";
-			}
-			 */
-		
-		$pdf->SetFont('stsongstdlight', 'B', 75);
-		$pdf->SetFillColor(255, 255, 255);
-		$pdf->SetTextColor(0,0,0);
-		$pdf->SetFont('helvetica', 'B', 75);
-		$pdf->Ln(40);
-	if (!empty($results[$n]["NameOnBadge"])){
-		$pdf->SetFont('helvetica', 'B', 55);
-		$pdf->Cell(0, 0, $NameOnBadge, 0, 1, 'C', 0, '', 1);
-		
-		$pdf->SetFont('stsongstdlight', 'B', 25);
+				
+				
+				$GivenName=$results[$n]["GivenName"];
+				echo "<h1>".$GivenName."</h1>";
+				
+				
+				//$CN_Company=$results[$n]["CN_Company"];
+				$FamilyName=$results[$n]["FamilyName"];
+				$EventYear=$results[$n]["EventYear"];
+				$Company=$results[$n]["Company"];
+				$ContactID=$results[$n]["ContactID"];
+				$InvitedByCompanyID=$results[$n]["InvitedByCompanyID"];
+				$HardCopy=$results[$n]["HardCopy"];
+				$Tutorial=$results[$n]["Tutorial"];
+				$Control=$results[$n]["Control"];
+				$Message=$results[$n]["Message"];
+				$Dinner=$results[$n]["Dinner"];
+				$type = $results[$n]["Type"];
+				$Email = $results[$n]["Email"];
+				//$ChineseName = $results[$n]["ChineseName"];
+				
+				$pdf->AddPage('P',$pageLayout);
+				$Dinnertext="";
+				
+				
+				
+					
+					 if($Tutorial==1){
+					$Tutorial="TUTORIAL";
+					}
+					else{
+					$Tutorial="";
+					}
+					 
+				//$pdf->Button('print', 30, 10, 'Print Badge', 'Print()', array('lineWidth'=>2, 'borderStyle'=>'solid', 'fillColor'=>array(255, 255, 255), 'strokeColor'=>array(0, 0, 0)));
 
 			
-	}
-	else if (!empty($results[$n]["GivenName"])){
-		$pdf->SetFont('helvetica', 'B', 55);
-		$pdf->Cell(0, 0, $GivenName, 0, 1, 'C', 0, '', 1);
-		
-		$pdf->SetFont('stsongstdlight', 'B', 25);
-		
-		
-	}
-	else {
-		$pdf->SetFont('stsongstdlight', 'B', 55);
-		
-		
-	}
-		$pdf->SetFont('stsongstdlight', 'B', 25);
-	
-		//$pdf->Cell(0, 0,$CN_Company, 0, 1, 'C', 0, '', 1);
-		$pdf->SetFont('helvetica', 'B', 25);
-		if(strlen($FamilyName)>8){
-		$pdf->SetFont('helvetica', 'B', 22);
-		}
-		$pdf->Cell(0, 0,$GivenName." ".$FamilyName, 0, 1, 'C', 0, '', 1);
-		$pdf->SetFont('helvetica', 'B', 25);
-		if(strlen($Company)>12){
-		$pdf->SetFont('helvetica', 'B', 17);
-		}
-		$pdf->Cell(0, 0,$Company, 0, 1, 'C', 0, '', 1);
-		
-		$pdf->Image($_SERVER["DOCUMENT_ROOT"].'/tmpqr/'.$n.'08.png', 7,115, 30, 30, 'PNG', '', '',false, 1000, '', false, false, 1, false, false, false);
-		
-		$pdf->SetFillColor(224,146,47);
-		$pdf->SetTextColor(255,255,255);
-		$pdf->SetFont('helvetica', 'B', 15);
-		$pdf->setCellPaddings(0, 6, 0, 0);
-		
-		$pdf->setCellPaddings(0, 0, 0, 0);
-		$pdf->SetFillColor(255,255,255);
-		$pdf->SetTextColor(0,0,0);
-		
-		if($type != 'Professional'){
-			if($type != "EXPOtiny"){
-		$pdf->SetFont('helvetica', 'B', 16);
-		$pdf->Cell(0, 0,'Ask me about:', 0, 1, 'C', 0, '', 1);
-			if(strlen($Message)>8){
-			$pdf->SetFont('helvetica', 'B', 22);
-		}
-		
-		$pdf->Cell(0, 0,$Message, 0, 1, 'C', 0, '', 1);
-		}
-		}
-		
-		if($type=="EXPOtiny"){
-		$pdf->SetFont('helvetica', '', 40);
-		$pdf->MultiCell(45, 20,"EXPO", 0, 'L', 0, 0, 45,120, true);
-		//$pdf->Rect(45, 123, 80, 30, 'F',array(), array(250,174,2));
-		}
-		
-		$pdf->SetFont('helvetica', '', 8);
-		
-		
-		//$pdf->MultiCell(90,10,$Dinnertext." ".$Tutorial." ".$Control." ".$i, 0, 'R', 0, 0, -8.5,140, true);
-	
-		$pdf->MultiCell(100,10,$Dinnertext." ".$Tutorial." ".$Control." ".$i, 0, 'R', 0, 0, -2.5,142, true);
-		//$pdf->MultiCell(90,10,$Tutorial." ".$Control." ".$i, 0, 'R', 0, 0, -8.5,144, true);
-		
-		$style = array(
-					'border' => 2,
+				$pdf->Ln(40);
+				
+				
+				//here is where we need the fonts
+				
+				// convert TTF font to TCPDF format and store it on the fonts folder
+			//$fontname = TCPDF_FONTS::addTTFfont(site_url('tcxcode/ThirdParty/NotoSerifKR-VariableFont_wght.ttf'), 'TrueTypeUnicode', '', 96);
+			//hysmyeoungjostdmedium
+			//hysmyeongjostdmedium.php
+			// use the font
+			if($EventYear == "Korea2024"){
+			$pdf->SetFont('cid0kr', '', 55,);
+			}
+			else if($EventYear == "China2024"){
+				$pdf->SetFont('cid0cs', '', 55,);
+			}
+			else{
+				$pdf->SetFont('helvetica', 'B', 55);
+			}
+			
+				$pdf->Cell(0, 0, $NameOnBadge, 0, 1, 'C', 0, '', 1);
+				
+			
+				$pdf->SetFont('helvetica', 'B', 25);
+				if(strlen($FamilyName)>8){
+				$pdf->SetFont('helvetica', 'B', 22);
+				}
+				$pdf->Cell(0, 0,$GivenName." ".$FamilyName, 0, 1, 'C', 0, '', 1);
+				$pdf->SetFont('helvetica', 'B', 25);
+				if(strlen($Company)>12){
+				$pdf->SetFont('helvetica', 'B', 17);
+				}
+				if(strlen($Company)>30){
+				$pdf->SetFont('helvetica', 'B', 12);
+				}
+				/*if(strlen($Company)>12){
+				$pdf->SetFont('stsongstdlight', 'B', 17);
+				}*/
+				$pdf->Cell(0, 0,$Company, 0, 1, 'C', 0, '', 1);
+				
+				
+				
+				$pdf->SetFillColor(224,146,47);
+				$pdf->SetTextColor(255,255,255);
+				$pdf->SetFont('helvetica', 'B', 15);
+				$pdf->setCellPaddings(0, 6, 0, 0);
+				
+				$pdf->setCellPaddings(0, 0, 0, 0);
+				$pdf->SetFillColor(255,255,255);
+				$pdf->SetTextColor(0,0,0);
+				
+				
+				
+			
+				
+				$pdf->SetFont('helvetica', '', 8);
+				$Control = substr($Control, -4);
+				
+			
+				$pdf->MultiCell(100,10,$Dinnertext." ".$Tutorial." ".$Control." ".$i, 0, 'L', 0, 0, 7,140, true);
+				
+				
+				// new style
+				$style = array(
+					'border' => 0,
 					'padding' => 'auto',
 					'fgcolor' => array(0,0,0),
 					'bgcolor' => array(255,255,255)
 				);
-				$code="Name: ".$GivenName." ".$FamilyName."\n"
+				
+				
+				$codeContents  = 'BEGIN:VCARD'."\n";
+				$codeContents .= 'VERSION:3.0'."\n";
+				$codeContents .= 'N:'.$FamilyName.";".$GivenName.";;;\n";
+				if (!empty($results[$n]["NameOnBadge"])){
+					$codeContents .= 'NICKNAME:'.$NameOnBadge."\n";
+				}
+				 				
+				$codeContents .= 'FN:'.$GivenName." ".$FamilyName."\n";
+				$codeContents .= 'EMAIL:'.$Email."\n"; 
+				$codeContents .= 'ORG:'.$Company."\n"; 
+				$codeContents .= 'END:VCARD'; 
+				
+				 $code="Name: ".$GivenName." ".$FamilyName."\n"
 				."Email: ".$Email."\n"
-				."Company: ".$Company;
+				."Company: ".$Company; 
+				
+				//$code="3880";
 				// QRCODE,H : QR-CODE Best error correction
-				//$pdf->write2DBarcode('Name:'.$GivenName.' '.$FamilyName.'<br>'.'Email:'.$Email.'<br>'.'Company: '.$Company, 'QRCODE,H', 7, 115, 30, 30, $style, 'N');
-		$pdf->write2DBarcode($code, 'QRCODE,H', 7, 115, 30, 30, $style, 'N');
-			
-		 $q++;
-		 		 
-	 
-	 
-	 
-	 }
+				//QR CODE IRA POSITION
+				//$pdf->write2DBarcode($codeContents, 'QRCODE,L', x position, y position, x size, y size, $style, 'N');
+				$pdf->write2DBarcode($codeContents, 'QRCODE,L', 7, 110, 30, 30, $style, 'N');
+				
+					
+				 $q++;
+						 
+			 
+			 
+			 
+			 }
 ob_clean();
 $pdf->Output('My-File-Name.pdf', 'I');
 //echo($pdf);
@@ -1778,7 +1762,9 @@ function Blankbadge($convention = 'testconx', $event = 'Mesa2025', $graphics = F
 			else{
 				$pdf->SetFont('helvetica', 'B', 55);
 			}
-			
+				if(strlen($NameOnBadge)>8){
+				$pdf->SetFont('helvetica', 'B', 50);
+				}
 				$pdf->Cell(0, 0, $NameOnBadge, 0, 1, 'C', 0, '', 1);
 				
 			
@@ -1786,14 +1772,23 @@ function Blankbadge($convention = 'testconx', $event = 'Mesa2025', $graphics = F
 				if(strlen($FamilyName)>8){
 				$pdf->SetFont('helvetica', 'B', 22);
 				}
+				$NameLength = strlen($FamilyName)+strlen($GivenName);
+				if(strlen($NameLength)>19){
+				$pdf->SetFont('helvetica', 'B', 18);
+				}
+				
 				$pdf->Cell(0, 0,$GivenName." ".$FamilyName, 0, 1, 'C', 0, '', 1);
 				$pdf->SetFont('helvetica', 'B', 25);
 				if(strlen($Company)>12){
 				$pdf->SetFont('helvetica', 'B', 17);
 				}
-				if(strlen($Company)>30){
+				if(strlen($Company)>20){
+				$pdf->SetFont('helvetica', 'B', 15);
+				}
+				if(strlen($Company)>25){
 				$pdf->SetFont('helvetica', 'B', 12);
 				}
+				
 				/*if(strlen($Company)>12){
 				$pdf->SetFont('stsongstdlight', 'B', 17);
 				}*/
