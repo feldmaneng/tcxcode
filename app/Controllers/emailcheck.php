f46aa8de5264646db8ca8080b982cd7c6c808a67
cmpvsolar <cmpvsolar@gmail.com>
2025-06-12 18:59:00 -0700

    test just bounce

--- app/Controllers/emailcheck.php
+++ app/Controllers/emailcheck.php
@@ -82,7 +82,7 @@ class emailcheck extends BaseController
 						//$ID[1] is the ContactID field
 						 if($ID[0]=="email address has been hard bounced from this audience and can't be imported."){
 							$rowb['EmailBounce']=1;
-							$rowb['Notes'].="Bounced - " . date("Y-m-d H:i:s");
+							$rowb['Notes'] = "Bounced - ";
 							//$rowb['Notes'].="Bounced - ".date("Y-m-d H:i:s");
 							if($count2 == 0){
 								$rowb['Active']=0;
@@ -95,7 +95,7 @@ class emailcheck extends BaseController
 						 if($ID[0]=="email address has been unsubscribed from this audience and can't be re-imported."){
 							$rowc['Exhibitinfo']='none';
 							$rowc['Techinfo']='none';
-							$rowb['Notes'].="Unsubscribed - ".date("Y-m-d H:i:s");
+							//$rowb['Notes'].="Unsubscribed - ".date("Y-m-d H:i:s");
 							if($count2 == 0){
 								$rowc['Active']=0;
 							}
