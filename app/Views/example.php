04eb6e40ae5ee5fb23cf4c2fced1042d0be774ca
cmpvsolar <cmpvsolar@gmail.com>
2025-06-22 22:43:20 -0700

    fix example

--- app/Views/example.php
+++ app/Views/example.php
@@ -1,77 +1,30 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<NotepadPlus>
-    <Session activeView="1">
-        <mainView activeIndex="0" />
-        <subView activeIndex="10">
-            <File firstVisibleLine="108" xOffset="0" scrollWidth="1130" startPos="6103" endPos="6103" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\tcxcode\app\Config\Routes.php" backupFilePath="" originalFileLastModifTimestamp="-1419684454" originalFileLastModifTimestampHigh="31187562" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
-            <File firstVisibleLine="582" xOffset="0" scrollWidth="1840" startPos="21305" endPos="21305" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\tcxcode\app\Controllers\Mailinglist.php" backupFilePath="" originalFileLastModifTimestamp="386412954" originalFileLastModifTimestampHigh="31185753" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no">
-                <Fold line="229" />
-                <Fold line="388" />
-                <Fold line="511" />
-                <Fold line="566" />
-                <Fold line="683" />
-                <Fold line="749" />
-                <Fold line="793" />
-                <Fold line="1004" />
-            </File>
-            <File firstVisibleLine="108" xOffset="308" scrollWidth="1910" startPos="8721" endPos="8721" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\tcxcode\app\Controllers\GeneralCert.php" backupFilePath="" originalFileLastModifTimestamp="-418887684" originalFileLastModifTimestampHigh="31186126" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
-            <File firstVisibleLine="42" xOffset="0" scrollWidth="2280" startPos="18868" endPos="19277" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\tcxcode\app\Controllers\Asiaguestchinese.php" backupFilePath="" originalFileLastModifTimestamp="-589789136" originalFileLastModifTimestampHigh="31187130" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no">
-                <Fold line="43" />
-                <Fold line="79" />
-                <Fold line="162" />
-                <Fold line="332" />
-                <Fold line="341" />
-                <Fold line="459" />
-                <Fold line="463" />
-                <Fold line="482" />
-                <Fold line="602" />
-                <Fold line="887" />
-                <Fold line="920" />
-                <Fold line="942" />
-                <Fold line="959" />
-                <Fold line="1006" />
-                <Fold line="1022" />
-                <Fold line="1038" />
-                <Fold line="1052" />
-                <Fold line="1171" />
-                <Fold line="1176" />
-                <Fold line="1181" />
-                <Fold line="1186" />
-                <Fold line="1192" />
-                <Fold line="1252" />
-                <Fold line="1265" />
-                <Fold line="1275" />
-                <Fold line="1289" />
-            </File>
-            <File firstVisibleLine="832" xOffset="0" scrollWidth="2280" startPos="26682" endPos="26682" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\tcxcode\app\Controllers\Asiaguest.php" backupFilePath="" originalFileLastModifTimestamp="-268609993" originalFileLastModifTimestampHigh="31081084" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no">
-                <Fold line="77" />
-                <Fold line="333" />
-            </File>
-            <File firstVisibleLine="0" xOffset="0" scrollWidth="1180" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\tcxcode\app\Views\one_company.php" backupFilePath="" originalFileLastModifTimestamp="-856196192" originalFileLastModifTimestampHigh="31130807" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
-            <File firstVisibleLine="333" xOffset="0" scrollWidth="2990" startPos="10183" endPos="10183" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\tcxcode\app\Controllers\Guest.php" backupFilePath="" originalFileLastModifTimestamp="757602857" originalFileLastModifTimestampHigh="31187799" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no">
-                <Fold line="373" />
-                <Fold line="398" />
-                <Fold line="434" />
-                <Fold line="517" />
-                <Fold line="687" />
-                <Fold line="696" />
-                <Fold line="863" />
-                <Fold line="1028" />
-                <Fold line="1032" />
-                <Fold line="1051" />
-                <Fold line="1156" />
-                <Fold line="1543" />
-                <Fold line="1548" />
-                <Fold line="1553" />
-                <Fold line="1558" />
-                <Fold line="1564" />
-                <Fold line="1598" />
-                <Fold line="1605" />
-                <Fold line="1624" />
-            </File>
-            <File firstVisibleLine="39" xOffset="0" scrollWidth="1420" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\tcxcode\app\Controllers\Database.php" backupFilePath="" originalFileLastModifTimestamp="-448183991" originalFileLastModifTimestampHigh="31184498" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
-            <File firstVisibleLine="0" xOffset="0" scrollWidth="1010" startPos="1915" endPos="1915" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\tcxcode\app\Views\testconx_template.php" backupFilePath="" originalFileLastModifTimestamp="-1234122039" originalFileLastModifTimestampHigh="31081293" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
-            <File firstVisibleLine="0" xOffset="0" scrollWidth="970" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="PHP" encoding="-1" userReadOnly="no" filename="C:\wamp64\www\TestConX\bitscode\application_secure\views\example.php" backupFilePath="" originalFileLastModifTimestamp="167742544" originalFileLastModifTimestampHigh="30974091" tabColourId="-1" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
-        </subView>
-    </Session>
-</NotepadPlus>
+!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php 
+foreach($css_files as $file): ?>
+	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+<?php endforeach; ?>
+<?php foreach($js_files as $file): ?>
+	<script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+</head>
+<body>
+	<div>
+		<a href='<?php echo site_url('examples/customers_management')?>'>Customers</a> |
+		<a href='<?php echo site_url('examples/orders_management')?>'>Orders</a> |
+		<a href='<?php echo site_url('examples/products_management')?>'>Products</a> |
+		<a href='<?php echo site_url('examples/offices_management')?>'>Offices</a> | 
+		<a href='<?php echo site_url('examples/employees_management')?>'>Employees</a> |		 
+		<a href='<?php echo site_url('examples/film_management')?>'>Films</a> |
+		<a href='<?php echo site_url('examples/multigrids')?>'>Multigrid [BETA]</a>
+		
+	</div>
+	<div style='height:20px;'></div>  
+    <div>
+		<?php echo $output; ?>
+    </div>
+</body>
+</html>
