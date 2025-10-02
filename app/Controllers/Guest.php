dc0c10fbdf61d8d0c4fa31c01b8a9c2abf13bc77
cmpvsolar <cmpvsolar@gmail.com>
2025-10-01 20:10:58 -0700

    update guest

--- app/Controllers/Guest.php
+++ app/Controllers/Guest.php
@@ -1473,13 +1473,23 @@ $builder->where('SecretKey', $secretKey);
 		// Not sure why we made another pass at the guest list...
 		// Turning off for now as we simply have found a dupe already
 		 if($rowcount == 1){
-				$sql = 'SELECT ContactID FROM guests Where EventYear = ? AND Email = ?;';
+			$db2 = db_connect('registration');
+
+			$builder2 = $db2->table('guests');
+
+			$builder2->where('EventYear', $_SESSION["EventYear"]);
+			$builder2->where('Email', $value);
+			$row2 = $query2->getRow();
+			$foundID =$row2->ContactID;
+			 
+			 
+			/* 	$sql = 'SELECT ContactID FROM guests Where EventYear = ? AND Email = ?;';
 
 				$query2 =$db2->query($sql,[$_SESSION["EventYear"],$value]);
 				$row2 = $query2->getRow();
 				
 				$foundID =$row2->ContactID;
-				
+				 */
 				
 						
 					if($foundID == $stateParameters->primaryKeyValue)
