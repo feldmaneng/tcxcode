b2838490aeee94105bd85af29bf891af19e30f4a
cmpvsolar <cmpvsolar@gmail.com>
2025-10-16 11:55:10 -0700

    made field readonly

--- app/Controllers/Guest.php
+++ app/Controllers/Guest.php
@@ -1321,7 +1321,7 @@ $builder->where('SecretKey', $secretKey);
 	'Mobile',
 	'ToPrint'
 	]);
-	
+	$crud->readOnlyFields(['ContactID']);
 	//$crud->fieldType('ContactID', 'invisible');
 	
 	/* $crud->readOnlyFields([
@@ -1476,10 +1476,10 @@ $builder->where('SecretKey', $secretKey);
 		// Not sure why we made another pass at the guest list...
 		// Turning off for now as we simply have found a dupe already
 		 if($rowcount == 1){
-			  /* if($fields['ContactID'] > 0)
+			   if($fields['ContactID'] > 0)
 			 {
 				 return true;
-			 }  */
+			 }  
 			/*  $db2 = db_connect('registration');
 
 			$builder2 = $db2->table('guests');
