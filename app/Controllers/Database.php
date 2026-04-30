68c8c4e511ce8151e8ec67058efa7cdab0470e29
Ira Feldman <iradave@gmail.com>
2026-04-29 20:54:20 -0700

    Update Database.php
    Missing field

--- app/Controllers/Database.php
+++ app/Controllers/Database.php
@@ -249,7 +249,7 @@ class Database extends BaseController {
         
         	
 		
-		$crud->columns (['CompanyID','Name','CN_Name','ParentID', 'IsParent']);
+		$crud->columns (['CompanyID','Name','CN_Name','ParentID', 'StandardName']);
 		
 		
 		
