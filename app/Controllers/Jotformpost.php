2fd96e2c01c22ab67a1f0842b9681712701f1884
cmpvsolar <cmpvsolar@gmail.com>
2026-08-18 05:41:27 -0700

    Update Jotformpost.php

--- app/Controllers/Jotformpost.php
+++ app/Controllers/Jotformpost.php
@@ -35,7 +35,7 @@ class Jotformpost extends BaseController {
 	
 	function postguest()
 	{
-		$eventYear = "Mesa2026"; //$_POST["eventYearID"];
+		$eventYear = "Korea2026"; //$_POST["eventYearID"];
 		
 		//die ("Reaching function");
 		
@@ -75,11 +75,11 @@ class Jotformpost extends BaseController {
 		}
 		//https://pci.jotform.com/form/260336014432142 add the current year ending url digits (that is 2026)
 		// Security check to make sure only certain forms are allowed
-		if (!str_contains('260336014432142, 260351610976154, 253415296943161, otherformnumbers', $_POST['formID'])) {
+		if (!str_contains('260336014432142,262158585479170, 260351610976154, 253415296943161, otherformnumbers', $_POST['formID'])) {
 			die ("Not authorized");
 		}
 		
-		if (($_POST['formID'] == "260336014432142") ||
+		if (($_POST['formID'] == "262158585479170") ||
 			($_POST['formID'] == "260351610976154") ) { //Test EXPO
 			$type = "EXPO";
 		} else {
