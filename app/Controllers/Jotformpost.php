743de133b7d3d6e82b9339ff671b15f1f316a752
cmpvsolar <cmpvsolar@gmail.com>
2026-08-25 08:46:30 -0700

    update

--- app/Controllers/Jotformpost.php
+++ app/Controllers/Jotformpost.php
@@ -75,16 +75,17 @@ class Jotformpost extends BaseController {
 		}
 		//https://pci.jotform.com/form/260336014432142 add the current year ending url digits (that is 2026)
 		// Security check to make sure only certain forms are allowed
-		if (!str_contains('262167847418164,262158585479170, 260351610976154, 253415296943161, otherformnumbers', $_POST['formID'])) {
+		if (!str_contains('262167847418164,262158585479170, 260351610976154, 262364454825159, otherformnumbers', $_POST['formID'])) {
 			die ("Not authorized");
 		}
 		
 		if (($_POST['formID'] == "261098599172167") ||
-			($_POST['formID'] == "262167847418164") ) 
+			($_POST['formID'] == "262167847418164") ||
+			 $_POST['formID'] == "262364454825159") 
 			{ //Test EXPO
 			$type = "Professional";
 		} 
-		if ($_POST['formID'] == "262158585479170")
+		if ($_POST['formID'] == "262158585479170" || $_POST['formID'] == "262364454825159")
 		{
 		$eventYear = "China2026";
 		
