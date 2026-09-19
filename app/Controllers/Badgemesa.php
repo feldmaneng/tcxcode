0660fea5fe81d97a3fe464a618d11d0957f84aa9
cmpvsolar <cmpvsolar@gmail.com>
2026-09-19 13:06:18 -0700

    update

--- app/Controllers/Badgemesa.php
+++ app/Controllers/Badgemesa.php
@@ -2753,9 +2753,9 @@ $db  = \Config\Database::connect('registration');
 				}*/
 				$pdf->Cell(0, 0,$Company, 0, 1, 'C', 0, '', 1);
 				
-				$pdf->SetFont('helvetica', 'B', 32);
+				$pdf->SetFont('helvetica', 'B', 64);
 				
-				$pdf->Cell(0, 0,$year, 0, 1, 'C', 0, '', 1);
+				//$pdf->Cell(0, 0,$year, 0, 1, 'C', 0, '', 1);
 				
 				$pdf->Cell(0, 0,"EXPO", 0, 1, 'C', 0, '', 1);
 				
